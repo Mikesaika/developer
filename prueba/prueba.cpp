a574ad3339b56b715dc5d923d5e1310853e11c09
Mikesaika <miguelnfuentes6@gmail.com>
2022-08-10 00:07:12 -0500

    MiPrimerComit

---
+++ prueba/prueba.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(int argc, char const *argv[])
+{
+    int n1,suma;
+    cout<<"ingresa numero";
+    cin>>n1;
+    suma=n1+80;
+    cout<<" "<<suma;
+    return 0;
+}
